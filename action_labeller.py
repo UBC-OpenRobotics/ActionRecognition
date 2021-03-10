54de37a0451ec87ffafe291e37bd707f56e82f66
Fquico1999 <fquico1999@gmail.com>
2021-03-10 12:01:46 -0800

    added data flag, added use CPU

--- action_labeller.py
+++ action_labeller.py
@@ -3,12 +3,16 @@ import cv2 as cv
 import argparse
 import numpy as np
 import time
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 from utils import choose_run_mode, load_pretrain_model, set_video_writer
 from Pose.pose_visualizer import TfPoseVisualizer
 from Action.recognizer import load_action_premodel, framewise_recognize
 
+
 parser = argparse.ArgumentParser(description='Action Recognition by OpenPose')
 parser.add_argument('--data', help='Path to data folder.')
+parser.add_argument('--video', help='Path to video.')
 args = parser.parse_args()
 
 estimator = load_pretrain_model('VGG_origin')
@@ -21,55 +25,81 @@ fps_count = 0
 run_timer = 0
 frame_count = 0
 
-cap = choose_run_mode(args)
-video_writer = set_video_writer(cap, write_fps=int(7.0))
-
-
-# # 保存关节数据的txt文件，用于训练过程(for training)
 f = open('origin_data.txt', 'a+')
 
-while cv.waitKey(1) < 0:
-    has_frame, show = cap.read()
-    if has_frame:
-        fps_count += 1
-        frame_count += 1
+cap = choose_run_mode(args)
+if cap:
+  video_writer = set_video_writer(cap, write_fps=int(7.0))
+  # # 保存关节数据的txt文件，用于训练过程(for training)
 
-        # pose estimation
-        humans = estimator.inference(show)
-        # get pose info
-        pose = TfPoseVisualizer.draw_pose_rgb(show, humans)  # return frame, joints, bboxes, xcenter
-        # recognize the action framewise
-        show = framewise_recognize(pose, action_classifier)
+  while cv.waitKey(1) < 0:
+      has_frame, show = cap.read()
+      if has_frame:
+          fps_count += 1
+          frame_count += 1
 
-        height, width = show.shape[:2]
-        # 显示实时FPS值
-        if (time.time() - start_time) > fps_interval:
-            # 计算这个interval过程中的帧数，若interval为1秒，则为FPS
-            realtime_fps = fps_count / (time.time() - start_time)
-            fps_count = 0  # 帧数清零
-            start_time = time.time()
-        fps_label = 'FPS:{0:.2f}'.format(realtime_fps)
-        cv.putText(show, fps_label, (width-160, 25), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+          # pose estimation
+          humans = estimator.inference(show)
+          # get pose info
+          pose = TfPoseVisualizer.draw_pose_rgb(show, humans)  # return frame, joints, bboxes, xcenter
+          # recognize the action framewise
+          show = framewise_recognize(pose, action_classifier)
 
-        # 显示检测到的人数
-        num_label = "Human: {0}".format(len(humans))
-        cv.putText(show, num_label, (5, height-45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+          height, width = show.shape[:2]
+          # 显示实时FPS值
+          if (time.time() - start_time) > fps_interval:
+              # 计算这个interval过程中的帧数，若interval为1秒，则为FPS
+              realtime_fps = fps_count / (time.time() - start_time)
+              fps_count = 0  # 帧数清零
+              start_time = time.time()
+          fps_label = 'FPS:{0:.2f}'.format(realtime_fps)
+          cv.putText(show, fps_label, (width-160, 25), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
 
-        # 显示目前的运行时长及总帧数
-        if frame_count == 1:
-            run_timer = time.time()
-        run_time = time.time() - run_timer
-        time_frame_label = '[Time:{0:.2f} | Frame:{1}]'.format(run_time, frame_count)
-        cv.putText(show, time_frame_label, (5, height-15), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+          # 显示检测到的人数
+          num_label = "Human: {0}".format(len(humans))
+          cv.putText(show, num_label, (5, height-45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
 
-        cv.imshow('Action Recognition based on OpenPose', show)
-        video_writer.write(show)
+          # 显示目前的运行时长及总帧数
+          if frame_count == 1:
+              run_timer = time.time()
+          run_time = time.time() - run_timer
+          time_frame_label = '[Time:{0:.2f} | Frame:{1}]'.format(run_time, frame_count)
+          cv.putText(show, time_frame_label, (5, height-15), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
 
-        # # 采集数据，用于训练过程(for training)
-        joints_norm_per_frame = np.array(pose[-1]).astype(np.str)
-        f.write(' '.join(joints_norm_per_frame))
-        f.write('\n')
+          cv.imshow('Action Recognition based on OpenPose', show)
+          video_writer.write(show)
 
-video_writer.release()
-cap.release()
+          # # 采集数据，用于训练过程(for training)
+          joints_norm_per_frame = np.array(pose[-1]).astype(np.str)
+          f.write(' '.join(joints_norm_per_frame))
+          f.write('\n')
+
+  video_writer.release()
+  cap.release()
+else:
+  data_dir  = args.data
+  assert os.path.exists(data_dir), '%s does not exist' % data_dir
+
+  for img_name in os.listdir(data_dir):
+    img_path = os.path.join(data_dir, img_name)
+    out_path = os.path.join('.', img_name)
+    show = cv.imread(img_path)
+
+    # pose estimation
+    humans = estimator.inference(show)
+    # get pose info
+    pose = TfPoseVisualizer.draw_pose_rgb(show, humans)  # return frame, joints, bboxes, xcenter
+    # recognize the action framewise
+    show = framewise_recognize(pose, action_classifier)
+    height, width = show.shape[:2]
+    num_label = "Human: {0}".format(len(humans))
+    cv.putText(show, num_label, (5, height-45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 3)
+
+    #cv.imwrite(out_path, show)
+    cv.imshow('Out', show)
+    cv.waitKey(1000)
+    joints_norm_per_frame = np.array(pose[-1]).astype(np.str)
+    f.write(' '.join(joints_norm_per_frame))
+    f.write('\n')
+    
 f.close()
